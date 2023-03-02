ba024fac52a6c64fcae3f3680baa3262ecd434b7
felipeachao <felipe.achao@gmail.com>
2023-03-01 21:37:08 -0400

    docs: npm i validationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.API_PATH, app, document);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.API_PORT);
 }
 bootstrap();
